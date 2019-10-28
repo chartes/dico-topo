c4da9899d556a11b2eac74a2588397eefaf40884
corentin_faye <corentin.faye@laposte.net>
2019-10-28 11:33:45 +0100

    refonte de add_commune pour être plus lisible

--- utils/Matchscript.py
+++ utils/Matchscript.py
@@ -1,7 +1,7 @@
 import csv
 d = {}
 #Le fichier doit contenir la liste des communes du département de l'INSEE 2018 qui sert de dictionnaire de référence
-with open("/home/corentink/Bureau/Dicotopo/DT62/listcommune.csv", newline='') as csvfile:
+with open("/home/corentink/Bureau/Dicotopo/DT73/listcommune.csv", newline='') as csvfile:
     ListcommunesInsee = csv.reader(csvfile, delimiter=',', quotechar='|')
     for communeInsee in ListcommunesInsee :
         d[communeInsee[1]] = communeInsee[0]
@@ -9,7 +9,7 @@ listresultat = []
 listrest = []
 correspondance = False
 #Le fichier doit contenir les communes restantes qui n'ont pas de match dans le département
-with open("/home/corentink/Bureau/Dicotopo/DT62/Rest2_prepared.csv", newline='') as csvfile:
+with open("/home/corentink/Bureau/Dicotopo/DT73/Rest_prepared.csv", newline='') as csvfile:
     rest = csv.reader(csvfile, delimiter=',', quotechar='|')
     for commune in rest :
         for NCC,INSEE in d.items():
@@ -24,7 +24,7 @@ with open("/home/corentink/Bureau/Dicotopo/DT62/Rest2_prepared.csv", newline='')
             listresultat.append(commune)
             correspondance = False
 #Il crée un premier fichier de résultats qui contient les matchs probables sur les substrings du nom des villes
-with open("/home/corentink/Bureau/Dicotopo/DT62/result2.csv","w", newline='') as csvfile:
+with open("/home/corentink/Bureau/Dicotopo/DT73/result2.csv","w", newline='') as csvfile:
     fichierfinal = csv.writer(csvfile, delimiter=',')
     for ligne in listresultat :
         fichierfinal.writerow(ligne)
@@ -44,13 +44,13 @@ for commune in listrest :
         correspondance = False
 
 #Il crée un fichier avec les résultats de match probable sur le premier mot du village
-with open("/home/corentink/Bureau/Dicotopo/DT62/result3.csv","w", newline='') as csvfile:
+with open("/home/corentink/Bureau/Dicotopo/DT73/result3.csv","w", newline='') as csvfile:
     fichierfinal = csv.writer(csvfile, delimiter=',')
     for ligne in listresultat2 :
         fichierfinal.writerow(ligne)
 
 #Il crée un fichier avec les communes qui n'ont matché avec aucune commune de l'INSEE 2018
-with open("/home/corentink/Bureau/Dicotopo/DT62/rest3.csv","w", newline='') as csvfile:
+with open("/home/corentink/Bureau/Dicotopo/DT73/rest3.csv","w", newline='') as csvfile:
     fichierfinal = csv.writer(csvfile, delimiter=',')
     for ligne in listrest2 :
         fichierfinal.writerow(ligne)
--- utils/add_commune.py
+++ utils/add_commune.py
@@ -1,21 +1,21 @@
 import xml.etree.ElementTree as ET
 import re
 import csv
+import unicodedata
 
-def add_commune (localisation):
+def add_commune (localisation, precision):
     """
     Ajout la balise commune pour les phrases ou la dernière entrée est cette commune
     :param localisation: contient les informations de la balise localisation du fichier xml
     :return:
     """
     commune = localisation.text.split()
-    #localisation.text = "commune de "
     localisation.text = ' '.join(commune[:-1])+" "
-    newcommune = ET.SubElement(localisation,"commune")
+    newcommune = ET.SubElement(localisation,"commune", precision=precision)
     newcommune.text = commune[-1]
 
 
-def add_commune_d (localisation):
+def add_commune_d(localisation, precision):
     """
     Ajout de la balise commune quand le mot est à la dernière place et séparé par un apostrophe
     :param localisation: contient les informations de la balise localisation du fichier xml
@@ -23,11 +23,10 @@ def add_commune_d (localisation):
     """
     commune = localisation.text.split("’")
     localisation.text = commune[0]+"’"
-    newcommune = ET.SubElement(localisation,"commune")
-    newcommune.text = commune[1]
+    newcommune = ET.SubElement(localisation,"commune", precision=precision)
+    newcommune.text = commune[-1]
 
-
-def add_communes (localisation):
+def add_communes (localisation, precision):
     """
 
     :param localisation: contient les informations de la balise localisation du fichier xml
@@ -37,7 +36,7 @@ def add_communes (localisation):
     co = re.split('([^a-zA-Z0-9À-ÿ\)\-])', localisation.text)
     localisation.text = ""
      #Ajout de la balise div dans la phrase final pour permettre un ajout aisé de la modification directement dans la balise localisation
-    phrase = "<div>"
+    phrase = "<tmp>"
     for mot in co:
         try:
             testadd = mot[0].isupper() and ")" not in mot
@@ -47,9 +46,10 @@ def add_communes (localisation):
             phrase = phrase + "<commune>" + mot + "</commune>"
         else :
             phrase = phrase + mot
-    phrase = phrase + "</div>"
+    phrase = phrase + "</tmp>"
      #Transforme phrase avec l'ensemble des informations en balise xml puis l'ajoute dans localisation. Il faudra supprimer les balises <div> et </div> dans le fichier final
     phrasexml = ET.fromstring(phrase)
+    phrasexml.set('precision', precision)
     localisation.append(phrasexml)
 
 def check_commune (localisation, d, article):
@@ -64,45 +64,51 @@ def check_commune (localisation, d, article):
     for nom, insee in d.items():
         if nom == localisation.text:
             localisation.text = ""
-            newcommune = ET.SubElement(localisation,"commune", insee=insee, precision="certain")
+            newcommune = ET.SubElement(localisation,"commune", insee=insee, precision="approximatif")
             newcommune.text = nom
             valide = True
     if valide is not True :
         print(article.attrib, localisation.text)
 
-
+#Donner l'emplacement du fichier XML du dictionnaire à utiliser pour la recherche
 tree= ET.parse("/home/corentink/Bureau/Dicotopo/DT44/DT44.xml")
 xml = tree.getroot()
-nbalone = 0
-nbcommune = 0
-nbelement = 0
+#Changer nom variable en indice commune précise
+listOfCommuneIndiceP = ["commune de", "commune du", "ville de"]
+listOfCommunedIndiceP = ["commune d’", "ville d’"]
+listofComumunesIndiceU = ["arrondissements", "cantons"]
+listofCommuneIndiceU = ["arrondissement de", "canton de", "canton du" ]
+listofCommunedIndiceU = ["arrondissement d'", "canton d'" ]
 nb = 0
 d = {}
  #Crée un dictionnaire qui contient le nom et le code insee pour chaque commune du dictionnaire
-with open("/home/corentink/Bureau/Dicotopo/DT44/FichierCorrection", newline='') as csvfile:
-    ListcommunesInsee = csv.reader(csvfile, delimiter='{', quotechar='|')
+with open("/home/corentink/Bureau/Dicotopo/Dossier_correction/DT44.csv", newline='') as csvfile:
+    ListcommunesInsee = csv.reader(csvfile, delimiter=',', quotechar='|')
     for communeInsee in ListcommunesInsee :
-        commune = communeInsee[1].split()
-        d[commune[0]] = communeInsee[4]
+        nCommune = unicodedata.normalize('NFKD',communeInsee[1]).encode('ascii','ignore').decode('utf-8')
+        d[nCommune] = communeInsee[5]
 
+#Utilisation de boucle for imbriqué pour pouvoir avoir accès à chaque niveau de l'information et faire les exports les plus précis possible
 for article in xml:
     for balises in article :
         if balises.tag == "definition":
             for localisation in balises :
                 if localisation.tag == "localisation":
-                    if "commune de" in localisation.text or "commune du" in localisation.text or "arrondissement de" in localisation.text or "canton de" in localisation.text or "canton du" in localisation.text or "village de" in localisation.text or "ville de" in localisation.text or "ville du" in localisation.text:
-                        add_commune(localisation)
-                    elif "commune d’" in localisation.text or "ville d’" in localisation.text or "village d’" in localisation.text or "arrondissement d’" in localisation.text or "canton d’" in localisation.text:
-                        add_commune_d(localisation)
-                    elif "communes" in localisation.text or "arrondissements" in localisation.text or "cantons" in localisation.text:
-                        add_communes(localisation)
-                    else :
-                        if len(localisation.text.split()) == 1:
-                            check_commune(localisation, d, article)
-                            nbalone = nbalone + 1
-                        else :
-                            print(article.attrib, localisation.text)
-                            nb = nb +1
+                    nb = nb +1
+                    for testCommune in listOfCommuneIndiceP  :
+                        if testCommune in localisation.text :
+                            add_commune(localisation, precision="certain")
+                    for testCommune in listOfCommunedIndiceP :
+                        if testCommune in localisation.text :
+                            add_commune_d(localisation, precision="certain")
+                    for testCommune in listofCommuneIndiceU :
+                        if testCommune in localisation.text :
+                            add_commune(localisation, precision="approximatif")
+                    for testCommune in listofComumunesIndiceU :
+                        if testCommune in localisation.text :
+                            add_communes(localisation, precision="approximatif")
+
+n = 0
 
 tree.write("output2.xml",encoding="UTF-8",xml_declaration=True)
 
