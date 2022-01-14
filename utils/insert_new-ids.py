c76ee7027de3a49c73d742fff1d6398770ceb556
architexte <architexte.git@gmail.com>
2022-01-14 19:38:35 +0100

    DT36, DT68,

--- utils/insert_new-ids.py
+++ utils/insert_new-ids.py
@@ -1,46 +1,35 @@
-import sys, codecs
+import sys
 import csv
 import re
-import time
 
-# utils % python insert_new-ids.py DT76
-
-start_time = time.time()
+# utils % python tp.py dt_id
 
 dt_id = sys.argv[1]
 dt_dir = '../data/'+dt_id+'/'
-dt_file_path = dt_dir+'/output6.xml'
-dt_ids_path  = dt_dir+'IDS_MAP.tsv'
+dt_f = dt_dir+'/output6.xml'
+mapping_f  = dt_dir+'IDS_MAP.tsv'
+mapping_d = {} # mapping dans un dict
 
-new_ids_dict = {}
+with open(mapping_f) as tsvfile:
+	tsvreader = csv.reader(tsvfile, delimiter="\t")
+	for row in tsvreader:
+		old_id = row[1]
+		new_id = row[0]
+		mapping_d[old_id] = new_id
 
-with open(dt_ids_path) as tsvfile:
-    tsvreader = csv.reader(tsvfile, delimiter="\t")
-    for row in tsvreader:
-        new_ids_dict[row[1]] = row[0]
-
-# search/replace in new file
-inFile = codecs.open(dt_file_path, "r", "utf-8")
-outFile = codecs.open(dt_dir + "output7.xml", "w", "utf-8")
+inFile = open(dt_f, 'r')
+outFile = open(dt_dir+'output7.xml', 'w')
 id_pattern = re.compile('article id="([^"]+)"')
 
 for line in inFile:
-    dt_old_id = re.search(id_pattern, line)
-    if dt_old_id is not None:
-        old_id = dt_old_id.group(1)
-        """
-        line = re.sub(
-            'id="({})"'.format(old_id),
-            'id="{}" old_id="\\1"'.format(new_ids_dict[old_id]),
-            line)
-        """
-        line = line.replace(
-            'id="{}"'.format(old_id),
-            'id="{}" old-id="{}"'.format(new_ids_dict[old_id], old_id))
-    #print(line)
-    outFile.write(line)
-    
+	dt_old_id = re.search(id_pattern, line)
+	if dt_old_id is not None:
+		old_id = dt_old_id.group(1)
+		line = line.replace(
+        	'id="{}"'.format(old_id),
+        	'id="{}" old-id="{}"'.format(mapping_d[old_id], old_id))
+	outFile.write(line)
+
+tsvfile.close()
 inFile.close()
 outFile.close()
-
-print("--- %s seconds ---" % (time.time() - start_time))
