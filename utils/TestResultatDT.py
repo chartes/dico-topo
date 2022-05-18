856bccb48ecc6ec5ff54a343b35865de2a79cadb
Corentinfaye <corentin.faye@laposte.net>
2022-05-18 15:16:23 +0200

    transformation of TestScript in CLI

--- utils/TestResultatDT.py
+++ utils/TestResultatDT.py
@@ -1,212 +1,228 @@
 import xml.etree.ElementTree as ET
 from lxml import etree
 import csv
+import click
 
-dep = "89"
+@click.command()
+@click.argument('dep', type=str)
+@click.argument('inputoriginal', type=str)
+@click.argument('inputfinal', type=str)
+def main(dep, inputoriginal, inputfinal):
+    """
+    DEP : Number of departement
 
-dir_path = "../data/"
-file_in = dir_path+"DT"+dep+"/DT"+dep+".xml"
-file_out = dir_path+"DT"+dep+"/output6.xml"
-file_result = "out/@DT"+dep+"_result"
+    INPUTORIGINAL : Original XML files of the DT
+
+    INPUTFINAL : Final XML files of the DT
+
+    output : csv in folder out @DT**DEP**_result.csv
+    """
+    dir_path = "../data/"
+    file_in = dir_path + "DT{0}/{1}".format(dep, inputoriginal)
+    file_out = dir_path+"DT{0}/{1}".format(dep, inputfinal)
+    file_result = "out/@DT"+dep+"_result"
 
 
-tree_original = etree.parse(file_in)
-tree = etree.parse(file_out)
+    tree_original = etree.parse(file_in)
+    tree = etree.parse(file_out)
 
-"""
-# sortir les alt labels
-for vedette in tree.xpath("//vedette[.//sm[position() > 1]]"):
-    print(etree.tostring(vedette, method="xml", encoding=str))
-exit()
-"""
+    """
+    # sortir les alt labels
+    for vedette in tree.xpath("//vedette[.//sm[position() > 1]]"):
+        print(etree.tostring(vedette, method="xml", encoding=str))
+    exit()
+    """
 
-#compte le nombre d'article dans le fichier xml
-count_article_in = 0
-count_article_out = 0
-for article in tree_original.xpath("article"):
-    count_article_in += 1
-for article in tree.xpath("article"):
-    count_article_out += 1
+    #compte le nombre d'article dans le fichier xml
+    count_article_in = 0
+    count_article_out = 0
+    for article in tree_original.xpath("article"):
+        count_article_in += 1
+    for article in tree.xpath("article"):
+        count_article_out += 1
 
-#Vérifier que le nombre de charactère d'un label soit inférieur à 30
-count_long_vedette_in = 0
-count_long_vedette_out = 0
-for article in tree_original.xpath("//article"):
-    for com in article.xpath(".//vedette//sm"):
-        if len(etree.tostring(com, method="text", encoding=str)) > 31:
-            print("in : " + article.get('id')+ " " + com.text)
-            count_long_vedette_in += 1
-for article in tree.xpath("//article"):
-    for com in article.xpath(".//vedette//sm"):
-        if len(etree.tostring(com, method="text", encoding=str)) > 31:
-            count_long_vedette_out += 1
-            print("out:" + article.get('id') + " " + etree.tostring(com, method="text", encoding=str))
+    #Vérifier que le nombre de charactère d'un label soit inférieur à 30
+    count_long_vedette_in = 0
+    count_long_vedette_out = 0
+    for article in tree_original.xpath("//article"):
+        for com in article.xpath(".//vedette//sm"):
+            if len(etree.tostring(com, method="text", encoding=str)) > 31:
+                print("in : " + article.get('id')+ " " + com.text)
+                count_long_vedette_in += 1
+    for article in tree.xpath("//article"):
+        for com in article.xpath(".//vedette//sm"):
+            if len(etree.tostring(com, method="text", encoding=str)) > 31:
+                count_long_vedette_out += 1
+                print("out:" + article.get('id') + " " + etree.tostring(com, method="text", encoding=str))
 
-#Compte le nombre de vedette
-count_vedette_in = 0
-count_vedette_out = 0
-for article in tree_original.xpath("//vedette//sm"):
-    count_vedette_in += 1
-for article in tree.xpath("//vedette//sm"):
-    count_vedette_out += 1
+    #Compte le nombre de vedette
+    count_vedette_in = 0
+    count_vedette_out = 0
+    for article in tree_original.xpath("//vedette//sm"):
+        count_vedette_in += 1
+    for article in tree.xpath("//vedette//sm"):
+        count_vedette_out += 1
 
-#compte le nombre d'article commune
-count_insee_in = 0
-count_insee_out = 0
-for commune in tree_original.xpath("//insee"):
-    count_insee_in += 1
-for article in tree.xpath("//insee"):
-    count_insee_out += 1
+    #compte le nombre d'article commune
+    count_insee_in = 0
+    count_insee_out = 0
+    for commune in tree_original.xpath("//insee"):
+        count_insee_in += 1
+    for article in tree.xpath("//insee"):
+        count_insee_out += 1
 
-#compte me nombre de commune avec l'article insee
-commune_insee_in = 0
-commune_insee_out = 0
-for commune in tree_original.xpath("//commune/@insee"):
-    commune_insee_in += 1
-for article in tree.xpath("//commune/@insee"):
-    commune_insee_out += 1
+    #compte me nombre de commune avec l'article insee
+    commune_insee_in = 0
+    commune_insee_out = 0
+    for commune in tree_original.xpath("//commune/@insee"):
+        commune_insee_in += 1
+    for article in tree.xpath("//commune/@insee"):
+        commune_insee_out += 1
 
-#Compte le nombre d'article_not_found (doit être à 0 pour la sortie)
-article_not_found_in = 0
-article_not_found_out = 0
-for commune in tree_original.xpath("//commune[@insee='article_not_found']"):
-    article_not_found_in += 1
-for article in tree.xpath("//commune[@insee='article_not_found']"):
-    article_not_found_out += 1
+    #Compte le nombre d'article_not_found (doit être à 0 pour la sortie)
+    article_not_found_in = 0
+    article_not_found_out = 0
+    for commune in tree_original.xpath("//commune[@insee='article_not_found']"):
+        article_not_found_in += 1
+    for article in tree.xpath("//commune[@insee='article_not_found']"):
+        article_not_found_out += 1
 
-#Compte le nombre de commentaire pour voir si on en a pas perdu
-commentary_in = 0
-commentary_out = 0
-for commune in tree_original.xpath("//commentaire"):
-    commentary_in += 1
-for article in tree.xpath("//commentaire"):
-    commentary_out += 1
+    #Compte le nombre de commentaire pour voir si on en a pas perdu
+    commentary_in = 0
+    commentary_out = 0
+    for commune in tree_original.xpath("//commentaire"):
+        commentary_in += 1
+    for article in tree.xpath("//commentaire"):
+        commentary_out += 1
 
-#Compte le nombre de formes anciennes, on ne doit pas en perdre
-forme_ancienne_in = 0
-forme_ancienne_out = 0
-for commune in tree_original.xpath("//forme_ancienne"):
-    forme_ancienne_in += 1
-for article in tree.xpath("//forme_ancienne"):
-    forme_ancienne_out += 1
+    #Compte le nombre de formes anciennes, on ne doit pas en perdre
+    forme_ancienne_in = 0
+    forme_ancienne_out = 0
+    for commune in tree_original.xpath("//forme_ancienne"):
+        forme_ancienne_in += 1
+    for article in tree.xpath("//forme_ancienne"):
+        forme_ancienne_out += 1
 
-#Compte le nombre de definition, on ne doit pas en perdre
-definition_in = 0
-definition_out = 0
-for commune in tree_original.xpath("//definition"):
-    definition_in += 1
-for article in tree.xpath("//definition"):
-    definition_out += 1
+    #Compte le nombre de definition, on ne doit pas en perdre
+    definition_in = 0
+    definition_out = 0
+    for commune in tree_original.xpath("//definition"):
+        definition_in += 1
+    for article in tree.xpath("//definition"):
+        definition_out += 1
 
-#Compte le nombre de typologie, on ne doit pas en perdre
-typologie_in = 0
-typologie_out = 0
-for commune in tree_original.xpath("//typologie"):
-    typologie_in += 1
-for article in tree.xpath("//typologie"):
-    typologie_out += 1
+    #Compte le nombre de typologie, on ne doit pas en perdre
+    typologie_in = 0
+    typologie_out = 0
+    for commune in tree_original.xpath("//typologie"):
+        typologie_in += 1
+    for article in tree.xpath("//typologie"):
+        typologie_out += 1
 
-#Compte le nombre d'article sans forme_ancienne
-article_not_forme_ancienne_in = 0
-article_not_forme_ancienne_out = 0
-for commune in tree_original.xpath("//article[not(forme_ancienne)]"):
-    article_not_forme_ancienne_in += 1
-for article in tree.xpath("//article[not(forme_ancienne)]"):
-    article_not_forme_ancienne_out += 1
+    #Compte le nombre d'article sans forme_ancienne
+    article_not_forme_ancienne_in = 0
+    article_not_forme_ancienne_out = 0
+    for commune in tree_original.xpath("//article[not(forme_ancienne)]"):
+        article_not_forme_ancienne_in += 1
+    for article in tree.xpath("//article[not(forme_ancienne)]"):
+        article_not_forme_ancienne_out += 1
 
-#Vérifie que tous les articles ont un id
-article_not_id_in = 0
-article_not_id_out = 0
-for commune in tree_original.xpath("//article[not(@id)]"):
-    article_not_id_in += 1
-for article in tree.xpath("//article[not(@id)]"):
-    article_not_id_out += 1
+    #Vérifie que tous les articles ont un id
+    article_not_id_in = 0
+    article_not_id_out = 0
+    for commune in tree_original.xpath("//article[not(@id)]"):
+        article_not_id_in += 1
+    for article in tree.xpath("//article[not(@id)]"):
+        article_not_id_out += 1
 
-#Vérifie que tous les id sont uniques dans un fichier
-id_unique_in = 0
-id_unique_out = 0
-num_id = ""
-num_id_out = ""
-for commune in tree_original.xpath("//article/@id"):
-    if num_id == commune :
-        print(commune)
-    else:
-        num_id = commune
-for article in tree.xpath("//article/@id"):
-    if num_id_out == article :
-        print(article)
-    else:
-        num_id_out = article
-
-#Controle que toutes les communes sont plus gros que 2
-count_commune_len_in = 0
-count_commune_len_out = 0
-for article in tree_original.xpath("//article"):
-    for com in article.xpath(".//commune"):
-        if len(etree.tostring(com, method="text", encoding=str)) < 3:
-            print("in:" + article.get('id') + etree.tostring(com, method="text", encoding=str))
-for article in tree.xpath("//article"):
-    for com in article.xpath(".//commune"):
-        if len(etree.tostring(com, method="text", encoding=str)) < 3:
-            print("out:" + article.get('id') + etree.tostring(com, method="text", encoding=str))
-#Controle que les valeurs de localisation sont identiques
-count_localisation_ok = 0
-dict_localisation_in_affichage = {}
-list_localisation_out =[]
-dict_localisation_in = {}
-for article in tree_original.xpath("//article"):
-    count_loc = 0
-    for loc in article.xpath(".//localisation"):
-        count_loc += 1
-        dict_localisation_in[article.get('id')+"-"+str(count_loc)] = "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n","").split())
-        dict_localisation_in_affichage[article.get('id')+"-"+str(count_loc)] = "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n",""))
-
-for article in tree.xpath("//article"):
-    count_loc = 0
-    for loc in article.xpath(".//localisation"):
-        count_loc += 1
-        if "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n","").split()) == dict_localisation_in[article.get('id')+"-"+str(count_loc)]:
-            continue
+    #Vérifie que tous les id sont uniques dans un fichier
+    id_unique_in = 0
+    id_unique_out = 0
+    num_id = ""
+    num_id_out = ""
+    for commune in tree_original.xpath("//article/@id"):
+        if num_id == commune :
+            print(commune)
         else:
-            count_localisation_ok += 1
-            list_localisation_out.append([article.get('id'),etree.tostring(loc, method="text", encoding=str), dict_localisation_in_affichage[article.get('id')+"-"+str(count_loc)]])
+            num_id = commune
+    for article in tree.xpath("//article/@id"):
+        if num_id_out == article :
+            print(article)
+        else:
+            num_id_out = article
 
-count_sup_out = 0
-#Controle que les vedettes  ne contiennet pas St ou S<sup>t
-for article in tree.xpath("//article"):
-    count_sup_out += 1
-    for sup in article.xpath(".//sm/sup"):
-        if "t" in sup.text :
-            print("problème de sup: ",article.get('id'))
-    for su in article.xpath(".//sm"):
-        if "St-" in etree.tostring(su, method="text", encoding=str):
-            print("problème ST : ", article.get("id"))
-        elif "St " in etree.tostring(su, method="text", encoding=str):
-            print("problème ST : ", article.get("id"))
+    #Controle que toutes les communes sont plus gros que 2
+    count_commune_len_in = 0
+    count_commune_len_out = 0
+    for article in tree_original.xpath("//article"):
+        for com in article.xpath(".//commune"):
+            if len(etree.tostring(com, method="text", encoding=str)) < 3:
+                print("in:" + article.get('id') + etree.tostring(com, method="text", encoding=str))
+    for article in tree.xpath("//article"):
+        for com in article.xpath(".//commune"):
+            if len(etree.tostring(com, method="text", encoding=str)) < 3:
+                print("out:" + article.get('id') + etree.tostring(com, method="text", encoding=str))
+    #Controle que les valeurs de localisation sont identiques
+    count_localisation_ok = 0
+    dict_localisation_in_affichage = {}
+    list_localisation_out =[]
+    dict_localisation_in = {}
+    for article in tree_original.xpath("//article"):
+        count_loc = 0
+        for loc in article.xpath(".//localisation"):
+            count_loc += 1
+            dict_localisation_in[article.get('id')+"-"+str(count_loc)] = "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n","").split())
+            dict_localisation_in_affichage[article.get('id')+"-"+str(count_loc)] = "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n",""))
+
+    for article in tree.xpath("//article"):
+        count_loc = 0
+        for loc in article.xpath(".//localisation"):
+            count_loc += 1
+            if len(article.get('id')) == 10:
+                if "".join(etree.tostring(loc, method="text", encoding=str).replace("’","'").replace("\n","").split()) == dict_localisation_in[article.get('id')+"-"+str(count_loc)]:
+                    continue
+                else:
+                    count_localisation_ok += 1
+                    list_localisation_out.append([article.get('id'),etree.tostring(loc, method="text", encoding=str)])
+
+    count_sup_out = 0
+    #Controle que les vedettes  ne contiennet pas St ou S<sup>t
+    for article in tree.xpath("//article"):
+        count_sup_out += 1
+        for sup in article.xpath(".//sm/sup"):
+            if "t" in sup.text :
+                print("problème de sup: ",article.get('id'))
+        for su in article.xpath(".//sm"):
+            if "St-" in etree.tostring(su, method="text", encoding=str):
+                print("problème ST : ", article.get("id"))
+            elif "St " in etree.tostring(su, method="text", encoding=str):
+                print("problème ST : ", article.get("id"))
 
 
 
-#Créer un fichier csv avec les différents résultats
-with open("{0}.csv".format(file_result), "w") as csvfile:
-    ListresultatTest = csv.writer(csvfile)
-    ListresultatTest.writerow (["", "DT_input", "DT_output(CF)"])
-    ListresultatTest.writerow (["place(article)", count_article_in, count_article_out])
-    ListresultatTest.writerow (["place.label(//vedette/sm)", count_vedette_in, count_vedette_out])
-    ListresultatTest.writerow (["place.label(//sm) > 30", count_long_vedette_in, count_long_vedette_out])
-    ListresultatTest.writerow (["place.commune_insee_code(//insee)", count_insee_in, count_insee_out])
-    ListresultatTest.writerow (["place.localization_commune_insee_code(//commune/@insee)", commune_insee_in, commune_insee_out])
-    ListresultatTest.writerow(["codes insee non résolu(//commune[@insee='article_not_found'])", article_not_found_in, article_not_found_out])
-    ListresultatTest.writerow(["place_comment.content (//commentaire)", commentary_in, commentary_out])
-    ListresultatTest.writerow(["place_old_label (//forme_ancienne)", forme_ancienne_in, forme_ancienne_out])
-    ListresultatTest.writerow(["place_description.content (//definition)", definition_in, definition_out])
-    ListresultatTest.writerow(["place_feature_type.term(//typologie)", typologie_in, typologie_out])
-    ListresultatTest.writerow(["place sans forme ancienne(//article[not(forme_ancienne)]) – total", article_not_forme_ancienne_in, article_not_forme_ancienne_out])
-    ListresultatTest.writerow(["place sans forme ancienne(//article[not(forme_ancienne)]) – %", article_not_forme_ancienne_in/count_article_in, article_not_forme_ancienne_out/count_article_out])
-    ListresultatTest.writerow(["article sans id (//article[not(@id)])",article_not_id_in, article_not_id_out])
+    #Créer un fichier csv avec les différents résultats
+    with open("{0}.csv".format(file_result), "w") as csvfile:
+        ListresultatTest = csv.writer(csvfile)
+        ListresultatTest.writerow (["", "DT_input", "DT_output(CF)"])
+        ListresultatTest.writerow (["place(article)", count_article_in, count_article_out])
+        ListresultatTest.writerow (["place.label(//vedette/sm)", count_vedette_in, count_vedette_out])
+        ListresultatTest.writerow (["place.label(//sm) > 30", count_long_vedette_in, count_long_vedette_out])
+        ListresultatTest.writerow (["place.commune_insee_code(//insee)", count_insee_in, count_insee_out])
+        ListresultatTest.writerow (["place.localization_commune_insee_code(//commune/@insee)", commune_insee_in, commune_insee_out])
+        ListresultatTest.writerow(["codes insee non résolu(//commune[@insee='article_not_found'])", article_not_found_in, article_not_found_out])
+        ListresultatTest.writerow(["place_comment.content (//commentaire)", commentary_in, commentary_out])
+        ListresultatTest.writerow(["place_old_label (//forme_ancienne)", forme_ancienne_in, forme_ancienne_out])
+        ListresultatTest.writerow(["place_description.content (//definition)", definition_in, definition_out])
+        ListresultatTest.writerow(["place_feature_type.term(//typologie)", typologie_in, typologie_out])
+        ListresultatTest.writerow(["place sans forme ancienne(//article[not(forme_ancienne)]) – total", article_not_forme_ancienne_in, article_not_forme_ancienne_out])
+        ListresultatTest.writerow(["place sans forme ancienne(//article[not(forme_ancienne)]) – %", article_not_forme_ancienne_in/count_article_in, article_not_forme_ancienne_out/count_article_out])
+        ListresultatTest.writerow(["article sans id (//article[not(@id)])",article_not_id_in, article_not_id_out])
 
-#Renvoie un fichier csv des localisation qui peuvent poser problème
-with open("{0}_commune.csv".format(file_result), "w") as csvfile:
-    ListCommuneResult = csv.writer(csvfile)
-    ListCommuneResult.writerow(["article", "ProblemeLocalisation ?"])
-    for com in list_localisation_out :
-        ListCommuneResult.writerow(com)
+    #Renvoie un fichier csv des localisation qui peuvent poser problème
+    with open("{0}_commune.csv".format(file_result), "w") as csvfile:
+        ListCommuneResult = csv.writer(csvfile)
+        ListCommuneResult.writerow(["article", "ProblemeLocalisation ?"])
+        for com in list_localisation_out :
+            ListCommuneResult.writerow(com)
+if __name__ == "__main__":
+    main()
--- utils/TestScript.py
+++ utils/TestScript.py
@@ -1,66 +1,80 @@
 import xml.etree.ElementTree as ET
 from lxml import etree
 import csv
+import click
 
-dep = "89"
-dir_path ="/home/corentin/Bureau/dico-topo/data/DT"
-xml_in = dir_path + dep + "/output6.xml"
-tsv_out = dir_path + dep + "/@result_validation_DT" + dep + ".xml"
-tree = etree.parse(xml_in)
 
-dict_commune = {}
-# création d'un dictionnaire des communes de 2011 pour pouvoir trouver le nom des communes qui datent de 2011 et non celle des DT qui peuvent avoir des noms de communes disparus ou pas forcément bien orthographier
-with open("dicotopo_insee-code.csv") as csvfile:
-    ListcommunesInsee = csv.reader(csvfile, delimiter='\t', quotechar='|')
-    for communeInsee in ListcommunesInsee:
-        dict_commune[communeInsee[0]] = communeInsee[1]
 
-#controle que les codes insee correspondent à celle des années 2011
-for commune in tree.xpath("//localisation/commune/@insee"):
-    if not commune in dict_commune:
-        print(commune)
+@click.command()
+@click.argument('dep', type=str)
+@click.argument('input', type=str)
+def main(dep, input):
+    """
+    DEP : Number of departement
 
-#compte le nombre d'article dans le fichier xml
-count_article = 0
-for article in tree.xpath("article"):
-    count_article += 1
-#compte le nombre d'article qui contiennent la balise insee et test si les données contenus dans chaque balise insee contiennent bien 5 caractères
-count_insee = 0
-count_leninsee =0
-for commune in tree.xpath("//insee"):
-    count_insee += 1
-    if len(commune.text) != 5:
-        count_leninsee += 1
-#compte le nombre d'article qui ne contient pas @type = commune et une balise insee
-count_notype = 0
-for commune in tree.xpath("//article[not(@type='commune')]/insee") :
-    count_notype += 1
-#compte le nombre d'article qui ne contient pas @type = commune et une balise insee
-count_noinsee = 0
-for commune in tree.xpath("//article[@type='commune'][not(insee)]") :
-    count_noinsee += 1
-#compte le nombre d'article qui contient une balise commune avec un attribut insee non vide et qui n'est pas une commune
-count_commune_insee = 0
-for commune in tree.xpath("//article[not(@type='commune')]/descendant::commune[not(@insee='')][1]"):
-    count_commune_insee += 1
-#compte le nombre d'article qui contient balise commune où manque l'attribut insee
-count_commune_inseemissing = 0
-for commune in tree.xpath("//localisation/commune[not(@insee)]"):
-    count_commune_inseemissing += 1
-#compte le nombre d'article qui contient balise commune avec un attribut insee vide et donc invalide
-count_commune_inseeempty = 0
-for commune in tree.xpath("//localisation/commune[@insee='']"):
-    count_commune_inseeempty += 1
-#contrôle que les entrées dans les attributs insee compte bien 5 chiffres
-count_invalideinsee = 0
-for commune in tree.xpath("//localisation/commune/@insee"):
-    if len(commune) != 5 :
-        print(commune)
-        count_invalideinsee += 1
+    INPUT : XML FILES of DT to test
 
-#Créer un fichier csv avec les différents résultats
-with open(tsv_out, "w") as csvfile:
-    ListresultatTest = csv.writer(csvfile)
-    ListresultatTest.writerow(["Nombre d'article", "Nombre de commune", "Code INSEE invalide", "Manque attribut type", "Manque balise insee", "Nombre article lié", "Balise commune sans attribut insee," ,"Balise commune avec attribut insee vide", "Attribut insee mal formé", "Pourcentage de réussite"])
-    ListresultatTest.writerow([count_article, count_insee, count_leninsee, count_notype, count_noinsee, count_insee+count_commune_insee, count_commune_inseemissing, count_commune_inseeempty, count_invalideinsee, ((count_insee + count_commune_insee)/count_article)*100])
+    output : csv in folder out @result_validation_DT**DEP**.csv
+    """
+    dir_path = "../data/"
+    xml_in = dir_path + "/DT{0}/{1}".format(dep, input)
+    tsv_out = "out/@result_validation_DT" + dep + ".csv"
+    tree = etree.parse(xml_in)
+    dict_commune = {}
+    # création d'un dictionnaire des communes de 2011 pour pouvoir trouver le nom des communes qui datent de 2011 et non celle des DT qui peuvent avoir des noms de communes disparus ou pas forcément bien orthographier
+    with open("Listedescommunes2011.csv") as csvfile:
+        ListcommunesInsee = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for communeInsee in ListcommunesInsee:
+            dict_commune[communeInsee[0]] = communeInsee[1]
 
+    #controle que les codes insee correspondent à celle des années 2011
+    for commune in tree.xpath("//localisation/commune/@insee"):
+        if not commune in dict_commune:
+            print(commune)
+
+    #compte le nombre d'article dans le fichier xml
+    count_article = 0
+    for article in tree.xpath("article"):
+        count_article += 1
+    #compte le nombre d'article qui contiennent la balise insee et test si les données contenus dans chaque balise insee contiennent bien 5 caractères
+    count_insee = 0
+    count_leninsee =0
+    for commune in tree.xpath("//insee"):
+        count_insee += 1
+        if len(commune.text) != 5:
+            count_leninsee += 1
+    #compte le nombre d'article qui ne contient pas @type = commune et une balise insee
+    count_notype = 0
+    for commune in tree.xpath("//article[not(@type='commune')]/insee") :
+        count_notype += 1
+    #compte le nombre d'article qui ne contient pas @type = commune et une balise insee
+    count_noinsee = 0
+    for commune in tree.xpath("//article[@type='commune'][not(insee)]") :
+        count_noinsee += 1
+    #compte le nombre d'article qui contient une balise commune avec un attribut insee non vide et qui n'est pas une commune
+    count_commune_insee = 0
+    for commune in tree.xpath("//article[not(@type='commune')]/descendant::commune[not(@insee='')][1]"):
+        count_commune_insee += 1
+    #compte le nombre d'article qui contient balise commune où manque l'attribut insee
+    count_commune_inseemissing = 0
+    for commune in tree.xpath("//localisation/commune[not(@insee)]"):
+        count_commune_inseemissing += 1
+    #compte le nombre d'article qui contient balise commune avec un attribut insee vide et donc invalide
+    count_commune_inseeempty = 0
+    for commune in tree.xpath("//localisation/commune[@insee='']"):
+        count_commune_inseeempty += 1
+    #contrôle que les entrées dans les attributs insee compte bien 5 chiffres
+    count_invalideinsee = 0
+    for commune in tree.xpath("//localisation/commune/@insee"):
+        if len(commune) != 5 :
+            print(commune)
+            count_invalideinsee += 1
+
+    #Créer un fichier csv avec les différents résultats
+    with open(tsv_out, "w") as csvfile:
+        ListresultatTest = csv.writer(csvfile)
+        ListresultatTest.writerow(["Nombre d'article", "Nombre de commune", "Code INSEE invalide", "Manque attribut type", "Manque balise insee", "Nombre article lié", "Balise commune sans attribut insee," ,"Balise commune avec attribut insee vide", "Attribut insee mal formé", "Pourcentage de réussite"])
+        ListresultatTest.writerow([count_article, count_insee, count_leninsee, count_notype, count_noinsee, count_insee+count_commune_insee, count_commune_inseemissing, count_commune_inseeempty, count_invalideinsee, ((count_insee + count_commune_insee)/count_article)*100])
+
+if __name__ == "__main__":
+    main()
